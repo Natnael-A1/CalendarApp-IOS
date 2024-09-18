f04218e748d26a42b944c64d6541a296aed46b3a
Natnael <nasmelash@captechconsulting.com>
2024-09-18 15:15:06 -0400

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Natneal Asmelash on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
